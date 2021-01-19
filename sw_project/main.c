69abe8d5547484534b640f8ab43979a3ef606b39
Piotr Jasinski <piotr.jasinski990@gmail.com>
2021-01-19 21:23:30 +0100

    dac, dma not working

--- sw_project/main.c
+++ sw_project/main.c
@@ -16,6 +16,8 @@ int calibration_cords[4][2] = {0};
 volatile uint32_t msTicks = 0;
 volatile uint32_t directionTicks = 0;
 
+const int samples_max = 1024;
+unsigned int samples[2] = {0, 1023<<5};
 enum Direction direction = UP;
 
 void takeTSCoords(int* x, int* y);
@@ -25,6 +27,14 @@ void SysTick_Handler(void) {  /* SysTick interrupt Handler. */
 	directionTicks++;
 }
 
+void DMA_IRQnHandler() {
+	LPC_GPDMACH0->DMACCSrcAddr = (int)samples;
+	LPC_GPDMACH0->DMACCDestAddr = (int)&LPC_DAC->DACR;
+	LPC_GPDMACH0->DMACCLLI = 0;
+	LPC_GPDMACH0->DMACCControl = 16 | (0b1 << 26) | (0b1 << 31);
+	LPC_GPDMACH0->DMACCConfig = 1 | (0b1<<11) | (7 << 1);
+	LPC_TIM1->IR = 1;
+}
 
 void drawCalibrationGrid() {
 	char buffer[32];
@@ -100,7 +110,7 @@ int main() {
 	lcdConfiguration();
 	init_ILI9325();
 	touchpanelInit();
-	
+
 	NVIC_SetPriority(EINT3_IRQn, 1);
 	SysTick_Config(SystemCoreClock / 1000);
 	
@@ -111,9 +121,48 @@ int main() {
 	clearScreen(LCDBlueSea);
 	NVIC_EnableIRQ(EINT3_IRQn);
 	
+	/* ------------------------------------------------------------ */
+	/* DAC */
+	/*
+	int freq = 440;
+	float dt = 1.0f/100;
+	for (int i = 0; i < 1024; ++i) {
+		samples[i] = (unsigned int)(((sin(i * dt) + 1)/2) * 1023) << 5;
+	}
+	*/
+	
+	PIN_Configure(0, 26, PIN_FUNC_2, PIN_PINMODE_TRISTATE, PIN_PINMODE_NORMAL);	// gpio dac 
+	// LPC_DAC->DACR
+	LPC_DAC->DACCTRL = 1 << 2 | 1 << 3; // timer enabled, dma burst request enabled 
+	LPC_DAC->DACCNTVAL = 12500; 
+	
+	/* ------------------------------------------------------------ */
+	/* DMA */
+	LPC_SC->PCONP |= 0b1 << 29;
+	LPC_GPDMA->DMACConfig = 0b1;
+	LPC_SC->DMAREQSEL = 0b1 << 2;
+	LPC_GPDMA->DMACIntTCClear = 1;
+	LPC_GPDMA->DMACIntErrClr = 1;
+ 
+	LPC_GPDMACH0->DMACCSrcAddr = (int)samples;
+	LPC_GPDMACH0->DMACCDestAddr = (int)&LPC_DAC->DACR;
+	LPC_GPDMACH0->DMACCLLI = 0;
+ 
+	LPC_GPDMACH0->DMACCControl = 16 | (0b1 << 26) | (0b1 << 31);     // cel nie podlega inkrementacji
+	// LPC_GPDMACH0->DMACCControl = 16 << 4; // szesnastobitowe rozmiary
+	// LPC_GPDMACH0->DMACCControl |= 0b1 << 31; // wlaczamy przerwanie
+ 
+	LPC_GPDMACH0->DMACCConfig = 1 | (0b1<<11) | (7 << 1);      // enable channel 0
+ // LPC_GPDMACH0->DMACCConfig |= 1 << 11;        // typ pamiec peryferium
+ // LPC_GPDMACH0->DMACCConfig |= 7 << 1; // zrodlo dac
+ // zerowanie bitu 14 clearuje IE
+	NVIC_EnableIRQ(DMA_IRQn);
+
+	/* ------------------------*/	
+
 	drawCalibrationGrid();
 	while (currentCalibrationCorner < 4);
-	
+			
 	while(1) {
 		clearScreen(LCDBlueSea);
 		int middleX = (calibration_cords[0][0] + calibration_cords[1][0]) / 2;
